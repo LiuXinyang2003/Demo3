fe71535a1c46922b1b82a08c1504b19c05bc7ac3
LiuXinyang <771093658@qq.com>
2024-05-05 19:53:11 +0800

    first commit

---
+++ src/main/java/com/LiuXinyang/Test1.java
@@ -0,0 +1,8 @@
+package com.LiuXinyang;
+
+public class Test1 {
+    public static void main(String[] args) {
+        System.out.println("This is a test1!");
+        System.out.println("This is a test2!");
+    }
+}
---
+++ src/main/java/com/LiuXinyang/Test2.java
@@ -0,0 +1,7 @@
+package com.LiuXinyang;
+
+public class Test2 {
+    public static void main(String[] args) {
+        System.out.println("测试用例1");
+    }
+}
